d4ecd0ad1f58257dab4c30e8e1e5afd4a757c713
Pam Cairo <pcairo_90@outlook.com>
2019-12-20 20:55:37 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Array Lab: Create, Retrieve, Update, Delete
+re u # Array Lab: Create, Retrieve, Update, Delete
 
 ## Overview
 
--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,55 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  puppies = ["Yorkshire Terrier","Maltese", "Chocolate Lab", "Gordon Setter"]
 end
 
 def add_element_to_end_of_array(array, element)
+  puppies = ["Yorkshire Terrier", "Maltese", "Chocolate Lab", "Gordon Setter"]
   
+  puppies << "arrays!"
+  
+  p puppies
+
 end
 
 def add_element_to_start_of_array(array, element)
+  puppies = ["Yorkshire Terrier", "Maltese", "Chocolate Lab", "Gordon Setter"]
+  
+  puppies.unshift("wow")
+  
+  p puppies
   
 end
 
 def remove_element_from_end_of_array(array)
+
+array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
   
+  array.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
+  
+ array[2]
   
 end
 
 def retrieve_first_element_from_array(array)
   
+  array[0]
+  
 end
 
 def retrieve_last_element_from_array(array)
+  
+  array[-1]
   
 end
