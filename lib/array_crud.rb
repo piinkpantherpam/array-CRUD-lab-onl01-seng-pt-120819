cd277c4d336aff0088aba6565bb3add1337787e4
Pam Cairo <pcairo_90@outlook.com>
2019-12-20 20:47:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,38 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  puppies = ["Yorkshire Terrier","Maltese", "Chocolate Lab", "Gordon Setter"]
 end
 
 def add_element_to_end_of_array(array, element)
+  puppies = ["Yorkshire Terrier", "Maltese", "Chocolate Lab", "Gordon Setter"]
   
+  puppies << "arrays!"
+  
+  p puppies
+
 end
 
 def add_element_to_start_of_array(array, element)
+  puppies = ["Yorkshire Terrier", "Maltese", "Chocolate Lab", "Gordon Setter"]
+  
+  puppies.unshift("wow")
+  
+  p puppies
   
 end
 
 def remove_element_from_end_of_array(array)
+
+array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  
+  array.shift
   
 end
 
